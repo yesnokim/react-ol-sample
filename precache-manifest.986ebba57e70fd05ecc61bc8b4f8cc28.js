978030f69eaab8224bc566635d4d643f16a017ba
ChrisKim <yesnokim@gmail.com>
2020-05-28 00:38:45 +0900

    Updates

---
+++ precache-manifest.986ebba57e70fd05ecc61bc8b4f8cc28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a238d20f6cf3c8538972d0ce179e4750",
+    "url": "/react-ol-sample/index.html"
+  },
+  {
+    "revision": "a53dd6c2c9d89c0277d9",
+    "url": "/react-ol-sample/static/css/2.db529fa1.chunk.css"
+  },
+  {
+    "revision": "a5338ae4c0bb9f98f48a",
+    "url": "/react-ol-sample/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a53dd6c2c9d89c0277d9",
+    "url": "/react-ol-sample/static/js/2.1d9b2d0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ol-sample/static/js/2.1d9b2d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5338ae4c0bb9f98f48a",
+    "url": "/react-ol-sample/static/js/main.6f12aabc.chunk.js"
+  },
+  {
+    "revision": "aa4c32fb421fe6240b51",
+    "url": "/react-ol-sample/static/js/runtime-main.bb0102f8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d87930a350fc9197efde96959944877.js"
+  "/react-ol-sample/precache-manifest.986ebba57e70fd05ecc61bc8b4f8cc28.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-ol-sample/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
